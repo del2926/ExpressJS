d7a5e0945ff2bbc4ed58fa30f3b8400483818dfc
del2926 <delilah.azahari@gmail.com>
2020-09-08 11:54:55 +0800

    update

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("public/images", express.static(path.join(__dirname, "images")));
 app.use(
   session({
     secret: "my secret",
