8a9b9f4f632f9aa77cb9042d52275bacda8c3ed0
del2926 <delilah.azahari@gmail.com>
2020-09-08 16:21:03 +0800

    final update

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,7 +10,7 @@ const Match = require("../models/match");
 const Product = require("../models/product");
 const Order = require("../models/order");
 
-const ITEMS_PER_PAGE = 4;
+const ITEMS_PER_PAGE = 3;
 
 exports.getProducts = (req, res, next) => {
   const page = +req.query.page || 1;
